66948b1771e9dc398487387f1b36ff45f9768dd9
Alexander Kutaladze <alexanderkutaladze@gmail.com>
2025-05-20 12:05:51 +0400

    update nvim colorscheme configuration

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,41 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = true, -- disables setting the background color.
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    })
+
+    -- setup must be called before loading
+    vim.cmd.colorscheme("catppuccin")
+  end,
+}
